3cc1ea50e0b48cf1598a9ea04a11049d01f12ba4
feli.pn <fpreussn@smail.uni-koeln.de>
2022-12-10 17:47:17 +0100

    Task 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,13 @@ public class FizzBuzz {
 	
 	public String convertNumber(int number) {
 		// Implement
-		return "";
+		if ((number%5==0)&&(number%7!=0)) {
+			return "Fizz";
+		} else if ((number%5!=0)&&(number%7==0)) {
+			return "Buzz";
+		} else if ((number%5==0)&&(number%7==0)) {
+			return "FizzBuzz";
+		}
+		else return "";
 	}
 }
